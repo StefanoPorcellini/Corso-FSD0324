3380cb790f4ea4e69c85dccf2e2aa1a9f5dc1669
StefanoPorcellini <stefano.porcellini@icloud.com>
2024-05-08 16:25:31 +0200

    modifiche

--- M2/W3/D3/Esercizio M2-W2-D3/assets/script/script.js
+++ M2/W3/D3/Esercizio M2-W2-D3/assets/script/script.js
@@ -19,13 +19,13 @@ const getBooks = function() {
             const cardContainer = document.getElementById('cardContainer')
             arrayOfCards.forEach((card) => {
             cardContainer.innerHTML += `
-                <div class="card border-info" style="width: 18rem;">
-                        <img src="${card.img}" class="card-img-top" alt="card title">
-                        <div class="card-body">
+                <div class="card border-info shadow" style="width: 18rem;">
+                        <img src="${card.img}" class="card-img-top overflow-hidden " alt="card title">
+                        <div class="card-body d-flex flex-column justify-content-between">
                           <h4 class="card-title">${card.title}</h4>
-                          <p class="card-text"><b>€ <span id="price">${card.price}</span></b></p>
                           
-                          <div class="d-flex justify-content-between">
+                          <div class="d-flex justify-content-between align-items-center mt-2">
+                                <p class="card-text m-0"><b>€ <span id="price">${card.price}</span></b></p>
                                 <a href="#" class="btn btn-success">Add to <span><i class="bi bi-cart4"></i></span></a>
                                 <a href="#" class="btn btn-danger" onclick="deleteCard(event)"><i class="bi bi-trash3"></i></a>
                           </div>
