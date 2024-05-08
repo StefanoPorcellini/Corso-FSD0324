b47051d8841993dae7dc3020112c31d573a66015
StefanoPorcellini <stefano.porcellini@icloud.com>
2024-05-08 15:53:59 +0200

    aggiunta bottone elimina

--- M2/W3/D3/Esercizio M2-W2-D3/assets/script/script.js
+++ M2/W3/D3/Esercizio M2-W2-D3/assets/script/script.js
@@ -1,7 +1,13 @@
 const currentYear = new Date().getFullYear()
 document.getElementById('curerntYear').innerText = currentYear
 
-
+const deleteCard = function (e) {
+    console.log('ELIMINO CARD', e)
+    // come faccio a capire QUALE degli n bottone elimina mi ha portato qui? devo capire QUALE card eliminare...
+    // e.target sarà il bottone che abbiamo cliccato
+    console.log(e.target.closest('.card'))
+    e.target.closest('.card').remove()
+}
 
 const getBooks = function() {
     fetch('https://striveschool-api.herokuapp.com/books')
@@ -25,7 +31,7 @@ const getBooks = function() {
                           
                           <div class="d-flex justify-content-between">
                                 <a href="#" class="btn btn-success">Add to <span><i class="bi bi-cart4"></i></span></a>
-                                <a href="#" class="btn btn-danger"><i class="bi bi-trash3"></i></a>
+                                <a href="#" class="btn btn-danger" onclick="deleteCard(event)"><i class="bi bi-trash3"></i></a>
                           </div>
                         </div>
                 </div>
