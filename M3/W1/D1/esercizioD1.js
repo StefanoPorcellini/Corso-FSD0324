d3f74a41eb61692f007f8f051f12901ff093ccf3
StefanoPorcellini <stefano.porcellini@icloud.com>
2024-05-21 15:00:06 +0200

    Esercizio M3-W1-D2

--- M3/W1/D1/esercizioD1.js
+++ M3/W1/D1/esercizioD1.js
@@ -1,24 +1,35 @@
 "use strict";
 const costoChiamata = 0.20;
 class User {
-    constructor(_nome, _cognome, credito, numeroChiamate) {
-        this.credito = credito;
-        this.numeroChiamate = numeroChiamate;
+    constructor(_nome, _cognome) {
+        this.credito = 0;
+        this.numeroChiamate = 0;
         this.nome = _nome;
         this.cognome = _cognome;
     }
     ricarica(importo) {
         this.credito += importo;
     }
+    chiamata(minuti) {
+        this.credito -= minuti * costoChiamata;
+        this.numeroChiamate += minuti;
+        console.log('Chiamata conclusa. Durata chiamata: ' + minuti + ' minuti. ', 'Costo chiamata: € ' + (minuti * costoChiamata) + '. Nuovo credito residuo: € ', this.credito);
+    }
+    chiama404() {
+        console.log('Ciamata al 404. Credito residuo: € ', this.credito);
+    }
+    creditoInsufficiente() {
+        if (this.credito < 0.2) {
+            console.log('ATTENZIONE! Credito Insufficiente');
+        }
+    }
 }
-const utente = new User('Mario', 'Rossi', 5, 15);
+const utente = new User('Mario', 'Rossi');
+console.log('utente', utente);
 utente.ricarica(10);
 console.log('Ricarica effettuata, nuovo credito residuo: € ', utente.credito);
-console.log('utente', utente);
-// let ricarica = function (creditoUtente:number | any, importo:number) {
-//     creditoUtente.credito += importo
-//     console.log('Ricarica effettuata, nuovo credito residuo: € ', creditoUtente.credito);
-// }
+utente.chiamata(5);
+utente.chiama404();
 // let chiamata = function (minuti:number):number{
 //     credito -= minuti*costoChiamata
 //     console.log('Chiamata conclusa, costo chiamata: € ' + (minuti*costoChiamata) + '. Nuovo credito residuo: € ', credito);    
--- M3/W1/D1/esercizioD1.ts
+++ M3/W1/D1/esercizioD1.ts
@@ -7,36 +7,54 @@ interface iSmartphone {
 
 
 class User implements iSmartphone{
+    credito: number = 0
+    numeroChiamate: number =0
     nome:string
     cognome:string
     constructor (
     _nome: string,
     _cognome: string,
-    public credito: number, 
-    public numeroChiamate?: number | undefined
     ){
         this.nome = _nome
         this.cognome = _cognome
     }
      ricarica(importo:number) {
         this.credito += importo
-}}
+    }
+     chiamata(minuti:number){
+             this.credito -= minuti*costoChiamata
+             this.numeroChiamate += minuti
+             console.log('Chiamata conclusa. Durata chiamata: ' + minuti + ' minuti. ' 'Costo chiamata: € ' + (minuti*costoChiamata) + '. Nuovo credito residuo: € ', this.credito)
+            }
+      chiama404(){
+             console.log('Ciamata al 404. Credito residuo: € ', this.credito);    
+        }
+      creditoInsufficiente(){
+        if (this.credito<0.2) {
+            console.log('ATTENZIONE! Credito Insufficiente');
+            
+        }
+      }
+    }
+             
 
 
-const utente = new User('Mario', 'Rossi', 5, 15)
+
+const utente = new User('Mario', 'Rossi')
+
+console.log('utente', utente);
 
 utente.ricarica(10)
 
 console.log('Ricarica effettuata, nuovo credito residuo: € ', utente.credito);
 
-console.log('utente', utente);
+utente.chiamata(5)
+
+utente.chiama404()
 
 
-// let ricarica = function (creditoUtente:number | any, importo:number) {
-//     creditoUtente.credito += importo
-//     console.log('Ricarica effettuata, nuovo credito residuo: € ', creditoUtente.credito);
-    
-// }
+
+
 
 // let chiamata = function (minuti:number):number{
 //     credito -= minuti*costoChiamata
